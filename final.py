474f54125cd6550730991df6e062435bc4d10dbb
cmtc00 <cosminnicolau008@gmail.com>
2023-11-28 17:35:35 +0200

    update

--- final.py
+++ final.py
@@ -1,127 +1,133 @@
-import cv2
-import dlib
+#python drowniness_yawn.py --webcam webcam_index
+
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+#from threading import Thread
+import numpy as np
+import argparse
+import imutils
 import time
-from scipy.spatial import distance
+import dlib
+import cv2
+import os
 
-# Indexare camera
-cap = cv2.VideoCapture(0)
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
 
-# Variabila pentru timer ochi inchisi
+    ear = (A + B) / (2.0 * C)
 
-ochi_inchisi = None
+    return ear
 
-timp_inchis = 2
+def final_ear(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# Detectare faciala cu ajutorul librariei dlib
-face_detector = dlib.get_frontal_face_detector()
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
 
-# Locatia din care se incarca landmark-urile faciale
-dlib_facelandmark = dlib.shape_predictor(r"shape_predictor_68_face_landmarks.dat")
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
 
-# Functia care calculeaza raportul de aspect al ochilor (Ratio)
-def Detectare_ochi(eye):
-    if len(eye) == 6:
-        poi_A = distance.euclidean(eye[1], eye[5])
-        poi_B = distance.euclidean(eye[2], eye[4])
-        poi_C = distance.euclidean(eye[0], eye[3])
-        aspect_ratio_ochi = (poi_A + poi_B) / (2 * poi_C)
-        return aspect_ratio_ochi
-    else:
-        return 0  # or any other default value
-    
-def Detectare_gura(mouth):
-    if len(mouth) == 12:
-        poi_A = distance.euclidean(mouth[2], mouth[10])
-        poi_B = distance.euclidean(mouth[4], mouth[8])
-        poi_C = distance.euclidean(mouth[0], mouth[6])
-        aspect_ratio_gura = (poi_A + poi_B) / (2 * poi_C)
-        return aspect_ratio_gura
-    else:
-        return 0  # or any other default value
+    ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
 
-# Main loop
+def lip_distance(shape):
+    top_lip = shape[50:53]
+    top_lip = np.concatenate((top_lip, shape[61:64]))
+
+    low_lip = shape[56:59]
+    low_lip = np.concatenate((low_lip, shape[65:68]))
+
+    top_mean = np.mean(top_lip, axis=0)
+    low_mean = np.mean(low_lip, axis=0)
+
+    distance = abs(top_mean[1] - low_mean[1])
+    return distance
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-w", "--webcam", type=int, default=0,
+                help="index of webcam on system")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 30
+YAWN_THRESH = 20
+COUNTER = 0
+
+print("-> Loading the predictor and detector...")
+#detector = dlib.get_frontal_face_detector()
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")    #Faster but less accurate
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+print("-> Starting Video Stream")
+vs = VideoStream(src=args["webcam"]).start()
+#vs= VideoStream(usePiCamera=True).start()       //For Raspberry Pi
+time.sleep(1.0)
+
 while True:
-    ret, frame = cap.read()
 
-    if not ret:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #rects = detector(gray, 0)
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+
+    #for rect in rects:
+    for (x, y, w, h) in rects:
+        rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
+        
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        eye = final_ear(shape)
+        ear = eye[0]
+        leftEye = eye [1]
+        rightEye = eye[2]
+
+        distance = lip_distance(shape)
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        lip = shape[48:60]
+        cv2.drawContours(frame, [lip], -1, (0, 255, 0), 1)
+
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        else:
+            COUNTER = 0
+
+        if (distance > YAWN_THRESH):
+            cv2.putText(frame, "Yawn Alert", (10, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        else:
+            cv2.putText(frame, "EYES: {:.2f}".format(ear), (300, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "YAWN: {:.2f}".format(distance), (300, 60),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
         break
 
-    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_detector(gray_scale)
-
-    for face in faces:
-        face_landmarks = dlib_facelandmark(gray_scale, face)
-        leftEye = []
-        rightEye = []
-        gura = []
-
-        # Left eye points (42 to 47)
-        for n in range(42, 48):
-            x = face_landmarks.part(n).x
-            y = face_landmarks.part(n).y
-            rightEye.append((x, y))
-            next_point = n + 1
-            if n == 47:
-                next_point = 42
-            x2 = face_landmarks.part(next_point).x
-            y2 = face_landmarks.part(next_point).y
-            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
-
-        # Right eye points (36 to 41)
-        for n in range(36, 42):
-            x = face_landmarks.part(n).x
-            y = face_landmarks.part(n).y
-            leftEye.append((x, y))
-            next_point = n + 1
-            if n == 41:
-                next_point = 36
-            x2 = face_landmarks.part(next_point).x
-            y2 = face_landmarks.part(next_point).y
-            cv2.line(frame, (x, y), (x2, y2), (255, 255, 0), 1)
-
-        # Landmark-uri pentru gura incep de la (48 - 59)
-        for n in range(48, 60):
-            x = face_landmarks.part(n).x
-            y = face_landmarks.part(n).y
-            gura.append((x, y))
-            next_point = n + 1
-            if n == 59:
-                next_point = 48
-            x2 = face_landmarks.part(next_point).x
-            y2 = face_landmarks.part(next_point).y
-            cv2.line(frame, (x, y), (x2, y2), (0, 0, 255), 1)
-
-        # Calculate aspect ratio for left and right eye
-        right_Eye = Detectare_ochi(rightEye)
-        left_Eye = Detectare_ochi(leftEye)
-        Eye_Rat = (left_Eye + right_Eye) / 2
-        gura = Detectare_gura(gura)
-
-        # Round off the value of the average mean of right and left eyes
-        Eye_Rat = round(Eye_Rat, 2)
-
-        # Threshold for drowsiness detection
-        if gura > 0.8:
-            cv2.putText(frame, "Cascare detectata", (30, 40),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 210), 3)
-        if Eye_Rat <= 0.12:
-            cv2.putText(frame, "Trezeste-te!", (50, 450),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 212), 3)
-        if 0.17 < Eye_Rat < 0.19:
-            cv2.putText(frame, "Oboseala detectata", (50, 100),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 210), 3)
-
-    cv2.imshow("Detector oboseala", frame)
-
-    # Buton de quit in caz ca nu merge stop window-ul
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
-
-# Release the camera and close all windows
-cap.release()
 cv2.destroyAllWindows()
-
-## Sursa pentru fisierul .dat cu landmark-uri 
-## https://github.com/italojs/facial-landmarks-recognition/blob/master/shape_predictor_68_face_landmarks.dat
+vs.stop()
+s
+##https://github.com/Arijit1080/Drowsiness-and-Yawn-Detection-with-voice-alert-using-Dlib
